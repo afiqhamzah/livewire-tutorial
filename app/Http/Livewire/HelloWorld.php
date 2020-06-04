4edacd7bb6ae54f7c463db00bfa40c6bb04a2d10
Afiq Hamzah <afiqhamzah@webimpian.com>
2020-06-04 17:29:23 +0800

    add reactivity

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -6,10 +6,18 @@ use Livewire\Component;
 
 class HelloWorld extends Component
 {
+    public $name = ' Jelly';
+    public $name_debounce = ' Jelly '.'debounce';
+    public $name_lazy = ' Jelly '.'lazy';
+
+    public $loud = false;
+    public $loudalot = false;
+    public $greeting = 'Hello';
+    public $greetingalot = ['Hello'];
+
+
     public function render()
     {
-        return view('livewire.hello-world', [
-            'name' => 'status'
-        ]);
+        return view('livewire.hello-world');
     }
 }
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,32 @@
 <div>
+    <input wire:model="name" type="text">
+    <input wire:model.debounce.1000ms="name_debounce" type="text">
+    <input wire:model.lazy="name_lazy" type="text">
+
     <h1>Hello {{ $name }}</h1>
+    <h1>Hello {{ strtoupper($name) }}</h1>
+
+    <h1>Hello debounced{{$name_debounce}}</h1>
+    <h1>Hello lazy{{$name_lazy}}</h1>
+
+    <select wire:model="greeting">
+        <option>Hello</option>
+        <option>Goodbye</option>
+        <option>Adios</option>
+    </select>
+
+
+    <select wire:model="greetingalot" multiple>
+        <option>Hello</option>
+        <option>Goodbye</option>
+        <option>Adios</option>
+    </select>
+
+    <input wire:model="loud" type="checkbox"></input>
+    <h1>{{ $greeting }} {{ $name }} @if ($loud) ! @endif<h1>
+
+
+    <input wire:model="loudalot" type="checkbox"></input>
+    <h1>{{ implode(', ', $greetingalot) }} {{ $name }} @if ($loudalot) ! @endif<h1>
+
 </div>
